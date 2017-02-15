cc1d29a8dcbbf8a7c52dfe57efab0c1387d5ba0a
Andrea Lovicu <alovicu@vivocha.com>
2017-02-15 18:13:17 +0100

    Added vivocha.ready check

--- views/js/sample.js
+++ views/js/sample.js
@@ -1,104 +1,107 @@
 $("#tabs").tabs().hide();
 var socket = io();
-var currentMedia = {
-  source: vivocha.channel().id,
-  load: 0.0,
-  paused: false,
-  media_sessions: {},
-  assigned: {},
-  assigned_count: 0,
-  pending: {},
-  pending_count: 0,
-  idle: {},
-  idle_count: 0,
-  available_media: ['chat','voice','video'],
-  busy_media: [],
-  weights: {}
-};
 var consoles = [];
 
-function manageConsole(econsole) {
-  console.log('VivochaEmbeddedConsole', econsole.id, 'ready');
-  var cid = econsole.id;
-  consoles.push(econsole);
+vivocha.ready().then(function () {
+  var currentMedia = {
+    source: vivocha.channel().id,
+    load: 0.0,
+    paused: false,
+    media_sessions: {},
+    assigned: {},
+    assigned_count: 0,
+    pending: {},
+    pending_count: 0,
+    idle: {},
+    idle_count: 0,
+    available_media: ['chat','voice','video'],
+    busy_media: [],
+    weights: {}
+  };
 
-  function onloadchange (update) {
-    console.info('loadchange', cid, update);
-  }
+  function manageConsole(econsole) {
+    console.log('VivochaEmbeddedConsole', econsole.id, 'ready');
+    var cid = econsole.id;
+    consoles.push(econsole);
 
-  function onmediachange (mediachange, diff) {
-    console.info('mediachange', cid, mediachange, diff);
-  }
-
-  econsole.channel().getAgentLoad().then(onloadchange);
-  econsole.channel().on('loadchange', onloadchange);
-  econsole.contact.getMedia().then(onmediachange);
-  econsole.contact.on('mediachange', onmediachange);
-  econsole.contact.on('text', function(text, from_id, from_nick) {
-    console.info((new Date()).toLocaleTimeString(), cid, (from_nick || from_id) + ':', text);
-    if ($('iframe#'+cid + ':visible').size() < 1) {
-      $('a[href="#_'+cid+'"]').addClass('ui-state-highlight');
+    function onloadchange (update) {
+      console.info('loadchange', cid, update);
     }
-  });
-  econsole.contact.on('localtext', function (text) {
-    console.info((new Date()).toLocaleTimeString(), cid, 'Agent:', text);
-  });
-  econsole.interface.on('windowclose', function() {
-    console.info("windowclose", cid);
-    $('._'+cid).remove();
-    if ( $('ul li').size() < 1) {
-      $("#tabs").hide();
-    }
-  });
-  econsole.interface.sendText('Hello world! This is a test message!');
-}
 
-socket.on('token', function(msg){
-  if (msg.token) {
-    var $container = $('<div id="_' + msg.cid + '" class="_' + msg.cid + ' contact" ></div>');
-    var $label = $('<li class="_' + msg.cid + '"></li>').append('<a href="#_' + msg.cid + '">' + msg.cid + '</a>');
-    vivocha.openConsole(msg.cid, msg.token, $container).then(manageConsole);
-    $('#tabs ul').append($label);
-    $('#tabs').append($container);
-    if ($('#tabs div').size() > 1) { // workaround to force the focus if there's only one contact
-      $("#tabs").show().tabs("refresh");
-    } else {
-      $("#tabs")
-        .tabs("destroy")
-        .tabs({
-          activate: function( event, ui ) {
-            ui.newTab.find('a').removeClass('ui-state-highlight');
-          }
-        })
-        .show();
+    function onmediachange (mediachange, diff) {
+      console.info('mediachange', cid, mediachange, diff);
     }
-  } else {
-    console.error('invalid token');
-  }
-});
 
-$('#pause').click(function () {
-  var $this = $(this);
-  if ($this.hasClass('disabled')) {
-    $this.removeClass('disabled');
-    currentMedia.paused = false;
-  } else {
-    $this.addClass('disabled');
-    currentMedia.paused = 'user';
-  }
-  vivocha.updateDeltaLoad(currentMedia, true);
-});
-$('#chat, #voice, #video').click(function () {
-  var $this = $(this);
-  var id = this.id;
-  if ($this.hasClass('disabled')) {
-    $this.removeClass('disabled');
-    currentMedia.available_media.push(id)
-  } else {
-    $this.addClass('disabled');
-    $.grep(currentMedia.available_media, function(value) {
-      return value != id;
+    econsole.channel().getAgentLoad().then(onloadchange);
+    econsole.channel().on('loadchange', onloadchange);
+    econsole.contact.getMedia().then(onmediachange);
+    econsole.contact.on('mediachange', onmediachange);
+    econsole.contact.on('text', function(text, from_id, from_nick) {
+      console.info((new Date()).toLocaleTimeString(), cid, (from_nick || from_id) + ':', text);
+      if ($('iframe#'+cid + ':visible').size() < 1) {
+        $('a[href="#_'+cid+'"]').addClass('ui-state-highlight');
+      }
     });
+    econsole.contact.on('localtext', function (text) {
+      console.info((new Date()).toLocaleTimeString(), cid, 'Agent:', text);
+    });
+    econsole.interface.on('windowclose', function() {
+      console.info("windowclose", cid);
+      $('._'+cid).remove();
+      if ( $('ul li').size() < 1) {
+        $("#tabs").hide();
+      }
+    });
+    econsole.interface.sendText('Hello world! This is a test message!');
   }
-  vivocha.updateDeltaLoad(currentMedia);
+
+  socket.on('token', function(msg){
+    if (msg.token) {
+      var $container = $('<div id="_' + msg.cid + '" class="_' + msg.cid + ' contact" ></div>');
+      var $label = $('<li class="_' + msg.cid + '"></li>').append('<a href="#_' + msg.cid + '">' + msg.cid + '</a>');
+      vivocha.openConsole(msg.cid, msg.token, $container).then(manageConsole);
+      $('#tabs ul').append($label);
+      $('#tabs').append($container);
+      if ($('#tabs div').size() > 1) { // workaround to force the focus if there's only one contact
+        $("#tabs").show().tabs("refresh");
+      } else {
+        $("#tabs")
+          .tabs("destroy")
+          .tabs({
+            activate: function( event, ui ) {
+              ui.newTab.find('a').removeClass('ui-state-highlight');
+            }
+          })
+          .show();
+      }
+    } else {
+      console.error('invalid token');
+    }
+  });
+
+  $('#pause').click(function () {
+    var $this = $(this);
+    if ($this.hasClass('disabled')) {
+      $this.removeClass('disabled');
+      currentMedia.paused = false;
+    } else {
+      $this.addClass('disabled');
+      currentMedia.paused = 'user';
+    }
+    vivocha.updateDeltaLoad(currentMedia, true);
+  });
+  $('#chat, #voice, #video').click(function () {
+    var $this = $(this);
+    var id = this.id;
+    if ($this.hasClass('disabled')) {
+      $this.removeClass('disabled');
+      currentMedia.available_media.push(id)
+    } else {
+      $this.addClass('disabled');
+      $.grep(currentMedia.available_media, function(value) {
+        return value != id;
+      });
+    }
+    vivocha.updateDeltaLoad(currentMedia);
+  });
 });
